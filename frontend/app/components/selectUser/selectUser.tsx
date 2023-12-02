3d910b23b7f13081351382ba439b52cb6fbea688
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2023-12-02 11:19:59 +0100

    aggiunto filtro per utenti

---
+++ frontend/app/components/selectUser/selectUser.tsx
@@ -0,0 +1,60 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+export default function SelectUser({ users }: { users: any }) {
+  const [filter, setFilter] = useState("");
+  const usersFiltered = users.filter((user: any) =>
+    user.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <div>
+      <p className="text-black bg-white p-1 m-2 font-bold text-5xl text-center">
+        OUR PLAYERS
+      </p>
+      <div className="flex justify-center mx-2">
+        <img src="./img/icons/lente-icon.png" className="bg-white p-2" alt="" />
+        <input
+          type="text"
+          placeholder="Filter by username"
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
+      <div className="grid grid-cols-4">
+        {usersFiltered?.map((user: any, index: any) => (
+          <a
+            className="col-span-4 m-2 md:col-span-1"
+            href={"./user/" + user.name}
+            key={user.id}
+          >
+            <div className="">
+              <div className="text-3xl font-bold text-center text-white bg-black">
+                {user.name}
+              </div>
+              <div>
+                {user.video_source ? (
+                  <Image
+                    className="img-card h-full w-full"
+                    src={"/img/screenshots/frame_" + user.dbName + ".jpg"}
+                    alt=""
+                    width={562}
+                    height={1000}
+                  />
+                ) : (
+                  <Image
+                    className="img-card h-full w-full"
+                    src={"/img/screenshots/frame_intro.jpg"}
+                    alt=""
+                    width={562}
+                    height={1000}
+                  />
+                )}
+              </div>
+            </div>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
--- frontend/app/user/page.tsx
+++ frontend/app/user/page.tsx
@@ -2,8 +2,8 @@ import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import React, { Suspense } from "react";
 import { getUsersByPuuid } from "../utils/riot/getUsersByPuuid";
-import Image from "next/image";
-import { getAllChamps } from "../utils/riot/getAllChamps";
+import SelectUser from "../components/selectUser/selectUser";
+
 export default async function User() {
   const supabase = createServerComponentClient({ cookies });
 
@@ -20,43 +20,8 @@ export default async function User() {
   return (
     <div className="">
       <div className="container mx-auto">
-        <p className="text-black bg-white p-1 m-2 font-bold text-5xl text-center">
-          OUR PLAYERS
-        </p>
         <Suspense fallback={<p className="bg-red-600">Loading USERS</p>}>
-          <div className="grid grid-cols-4">
-            {users?.map((user, index) => (
-              <a
-                className="col-span-4 m-2 md:col-span-1"
-                href={"./user/" + user.name}
-              >
-                <div className="" key={user.id}>
-                  <div className="text-3xl font-bold text-center text-white bg-black">
-                    {user.name}
-                  </div>
-                  <div>
-                    {user.video_source ? (
-                      <Image
-                        className="img-card h-full w-full"
-                        src={"/img/screenshots/frame_" + user.dbName + ".jpg"}
-                        alt=""
-                        width={562}
-                        height={1000}
-                      />
-                    ) : (
-                      <Image
-                        className="img-card h-full w-full"
-                        src={"/img/screenshots/frame_intro.jpg"}
-                        alt=""
-                        width={562}
-                        height={1000}
-                      />
-                    )}
-                  </div>
-                </div>
-              </a>
-            ))}
-          </div>
+          <SelectUser users={users}></SelectUser>
         </Suspense>
       </div>
     </div>
