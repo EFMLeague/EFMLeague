fe5655f0ea243e90a815093c8654ecda20af7549
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2023-12-07 15:02:01 +0100

    prima prova draft

--- frontend/app/draft/[name]/page.tsx
+++ frontend/app/draft/[name]/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
 import Image from "next/image";
 
-const socket: Socket = io("http://localhost:3001", {
+const socket: Socket = io("https://backend-efmleague.onrender.com:3001", {
   path: "/draft/",
 });
 
