fda0c33972ce219c30c4625f3e795ed8f0f509d6
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2023-12-07 14:39:04 +0100

    first commit per backend

---
+++ backend/server.js
@@ -0,0 +1,104 @@
+// server.js
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+// Il codice del tuo server Socket.IO
+const io = new Server(server, {
+  path: "/draft/",
+  cors: {
+    origin: "https://www.efmleague.com",
+    methods: ["GET", "POST"],
+  },
+});
+
+const generateRandomLink = () => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const linkLength = 10;
+  let randomLink = "";
+
+  for (let i = 0; i < linkLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomLink += characters[randomIndex];
+  }
+
+  return randomLink + "$";
+};
+
+const printRooms = (active) => {
+  active.forEach((element) => {
+    console.log(element);
+  });
+};
+var activeRooms = [];
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on("create_room", (data) => {
+    console.log("creazione room richiesta");
+    console.log(data.nameBlue);
+    const newRooms = {
+      roomNumber: generateRandomLink(),
+      blue: generateRandomLink(),
+      red: generateRandomLink(),
+      blueName: data.nameBlue === "" ? "Blue" : data.nameBlue,
+      redName: data.nameRed === "" ? "Red" : data.nameRed,
+    };
+    activeRooms.push(newRooms);
+    printRooms(activeRooms);
+    socket.emit("message_received", newRooms);
+  });
+
+  socket.on("print_room", () => {
+    console.log("stampando le rooms");
+    printRooms(activeRooms);
+    console.log("fine stampa");
+  });
+
+  socket.on("join_room", (room) => {
+    const res = activeRooms.find(({ roomNumber }) => roomNumber === room);
+
+    if (activeRooms.find(({ roomNumber }) => roomNumber === room)) {
+      socket.join(room);
+      console.log(`User ${socket.id} joined room: ${room}`);
+      io.to(room).emit("receive_room", {
+        teamBlue: res.blueName,
+        teamRed: res.redName,
+      });
+    } else {
+      console.log(` la room: ${room} non esiste`);
+    }
+  });
+
+  socket.on("send_message", (data) => {
+    const res = activeRooms.find(({ roomNumber }) => roomNumber === data.room);
+
+    if (data.passwordSide === "") {
+      console.log("l'utente non può inviare messaggi");
+    }
+    if (res.red === data.passwordSide || res.blue === data.passwordSide) {
+      // Verifica se il mittente è nella stanza come "red" o "blue"
+      const sender = res.red === data.passwordSide ? "red" : "blue";
+
+      io.to(data.room).emit("receive_message", {
+        message: data.message,
+        sender: sender,
+      });
+      console.log(data.message + "<->" + sender);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is running on port 3001");
+});
---
+++ frontend/app/draft/[name]/page.tsx
@@ -0,0 +1,70 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Socket, io } from "socket.io-client";
+import Image from "next/image";
+
+const socket: Socket = io("http://localhost:3001", {
+  path: "/draft/",
+});
+
+const url = window.location.href;
+const startIndex = url.indexOf("/draft/");
+const endIndex = url.indexOf("$");
+var room = url.substring(startIndex + 7, endIndex + 1);
+var passwordSide = url.substring(endIndex + 1);
+
+if (room.length) socket.emit("join_room", room);
+
+export default function page() {
+  const [squad, setSquad] = useState({ blueSide: "", redSide: "" });
+  const [message, setMessage] = useState("");
+
+  const [messageReceived, setMessageReceived] = useState("");
+
+  useEffect(() => {
+    socket.on(
+      "receive_message",
+      (data: { message: string; sender: string }) => {
+        setMessageReceived(data.message);
+      }
+    );
+    socket.on("receive_room", (data: { teamBlue: string; teamRed: string }) => {
+      setSquad({ blueSide: data.teamBlue, redSide: data.teamRed });
+    });
+    console.log(socket);
+  }, [socket]);
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room, passwordSide });
+  };
+
+  return (
+    <div className="bg-blue-gray-900 h-screen">
+      <div className="flex justify-between pt-10">
+        <p className="text-3xl font-bold bg-blue-500 px-10">{squad.blueSide}</p>
+        <p className="text-3xl font-bold bg-red-500 px-10">{squad.redSide}</p>
+      </div>
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+        className="border-2 border-black"
+      />
+
+      <button onClick={sendMessage}> Send Message</button>
+      <p>{messageReceived}</p>
+      <img
+        src={
+          "https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/" +
+          messageReceived +
+          ".png"
+        }
+        alt=""
+        width={48}
+        height={48}
+        className="h-12 w-12"
+      />
+    </div>
+  );
+}
