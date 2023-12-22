31a25a3c74e63f3344cce0fc3a4ad24306b8a777
Francesco Nicolo <nicolo.francesco.fn@gmail.com>
2023-12-22 16:36:22 +0100

    aggiunto termini e condizioni

---
+++ frontend/app/terms/page.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="bg-white container mx-auto">
+      <p className="text-lg ">
+        Termini e Condizioni di EFM League
+        <br />
+        Ultimo aggiornamento: [Data]
+        <br />
+        Benvenuto su [https://www.efmleague.com] ("Sito"). Utilizzando il Sito e
+        partecipando agli eventi offerti, accetti di essere vincolato dai
+        seguenti Termini e Condizioni:
+        <br />
+        1. Registrazione Utente: La registrazione al nostro sito è opzionale.
+        Durante il processo di registrazione, raccogliamo e crittografiamo
+        l'email e la password per migliorare la tua esperienza sul sito.
+        <br />
+        2. Privacy: La nostra politica sulla privacy è disponibile all'indirizzo
+        [https://www.iubenda.com/privacy-policy/35318516]. I dati degli utenti
+        vengono utilizzati solo per verificare l'identità e non vengono
+        condivisi con terze parti.
+        <br />
+        3. Proprietà Intellettuale: I diritti d'autore per i contenuti
+        pubblicati su questo sito appartengono a Francesco Nicolo'. L'uso non
+        autorizzato di tali contenuti è vietato.
+        <br />
+        4. Condizioni di Utilizzo: Non ci sono limitazioni sull'età per
+        l'utilizzo del sito. Gli utenti devono rispettare le leggi locali e le
+        regole del sito durante la partecipazione agli eventi.
+        <br />
+        5. Contenuti Generati dagli Utenti: Gli utenti sono responsabili dei
+        contenuti generati e condivisi. Il Sito si riserva il diritto di
+        rimuovere contenuti inappropriati o violazioni delle regole.
+        <br />
+        6. Modifiche ai Termini e Condizioni: I Termini e Condizioni possono
+        essere modificati. Gli utenti saranno informati tramite email delle
+        modifiche e sono tenuti a riesaminare regolarmente i Termini e
+        Condizioni.
+        <br />
+        7. Limitazioni di Responsabilità: Il Sito non è responsabile di
+        eventuali danni o perdite subite dagli utenti durante la partecipazione
+        agli eventi.
+        <br />
+        8. Contatti: Per qualsiasi domanda o richiesta, contattaci via email
+        all'indirizzo [support@efmleague.com].
+      </p>
+    </div>
+  );
+}
