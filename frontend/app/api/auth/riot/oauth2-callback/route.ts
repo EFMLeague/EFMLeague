878da2b5bc6e6cf7a395cfc5548555de155e22d5
stefanopiccoli <stefano.piccoli2800@gmail.com>
2023-12-22 22:52:55 +0100

    Token

--- frontend/app/api/auth/riot/oauth2-callback/route.ts
+++ frontend/app/api/auth/riot/oauth2-callback/route.ts
@@ -2,13 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   var accessCode = req.nextUrl.searchParams.get("code");
-  const appCallbackUrl= "https://www.efmleague.com/api/auth/riot/oauth2-callback"
-  const tokenUrl="https://auth.riotgames.com/token"
+  const appCallbackUrl =
+    "https://www.efmleague.com/api/auth/riot/oauth2-callback";
+  const tokenUrl = "https://auth.riotgames.com/token";
   try {
     const response = await fetch(tokenUrl, {
       method: "POST",
       headers: {
-        Authorization: "Basic " + btoa(process.env.RIOT_AUTH_CLIENT_ID + ":" + process.env.RIOT_AUTH_CLIENT_SECRET),
+        Authorization:
+          "Basic " +
+          btoa(
+            process.env.RIOT_AUTH_CLIENT_ID +
+              ":" +
+              process.env.RIOT_AUTH_CLIENT_SECRET
+          ),
         "Content-Type": "application/x-www-form-urlencoded",
       },
       body: JSON.stringify({
@@ -18,24 +25,31 @@ export async function GET(req: NextRequest) {
       }),
     });
 
-    if (!response.ok) {
-      throw new Error("Errore nella richiesta");
-    }
+    // if (!response.ok) {
+    //   throw new Error("Errore nella richiesta");
+    // }
 
-    const data = await response.json();
+    const responseBody = await response.text();
+    const payload = JSON.parse(responseBody);
+
+    const tokens = {
+      refresh_token: payload.refresh_token,
+      id_token: payload.id_token,
+      access_token: payload.access_token,
+    };
+
+    // const data = await response.json();
     console.log("OK");
-    
-    console.log(data);
-    
-    return NextResponse.json({data}, {status:200})
-    console.log(data);
+
+    console.log(tokens);
+
+    return NextResponse.json({ tokens }, { status: 200 });
   } catch (error) {
     // Gestisci l'errore
     console.log("ERRORE");
-    
+
     console.log(error);
 
-    return NextResponse.json({error}, {status:500})
-    
+    return NextResponse.json({ error }, { status: 500 });
   }
 }
