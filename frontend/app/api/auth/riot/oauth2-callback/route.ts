37451f25303a8caeac2ecee59fb5583e6b35ec8c
stefanopiccoli <stefano.piccoli2800@gmail.com>
2023-12-23 17:47:48 +0100

    RiotToken on Client

--- frontend/app/api/auth/riot/oauth2-callback/route.ts
+++ frontend/app/api/auth/riot/oauth2-callback/route.ts
@@ -4,15 +4,16 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const accessCode = req.nextUrl.searchParams.get("code") as string;
 
-  const appCallbackUrl =
-    "https://www.efmleague.com/login/outcome";
+  const appCallbackUrl = "https://efmleague.com/login/outcome";
   const tokenUrl = "https://auth.riotgames.com/token";
-  // try {
-    const formData = new URLSearchParams();
-    formData.append("grant_type", "authorization_code");
-    formData.append("code", accessCode as string);
-    formData.append("redirect_uri", appCallbackUrl);
-    const tokens = await axios.post(tokenUrl, formData, {
+  const formData = new URLSearchParams();
+  formData.append("grant_type", "authorization_code");
+  formData.append("code", accessCode as string);
+  formData.append("redirect_uri", appCallbackUrl);
+  try {
+    const response = await fetch(tokenUrl, {
+      method: "POST",
+      body: formData,
       headers: {
         Authorization:
           "Basic " +
@@ -23,31 +24,11 @@ export async function GET(req: NextRequest) {
           ).toString("base64"),
       },
     });
+    const tokens = await response.json();
+    console.log(tokens);
 
-    // if (!response.ok) {
-    //   throw new Error("Errore nella richiesta");
-    // }
-
-    // const data = await response.json();
-    console.log("OK");
-    // console.log(tokens.data);
-    console.log(tokens.data);
-
-    return NextResponse.json({ tokens:tokens.data }, { status: 200 });
-  // } catch (error) {
-  //   console.log("ERRORE");
-  //   console.log(error);
-  //   console.log(
-  //     "Basic " +
-  //       Buffer.from(
-  //         process.env.RIOT_AUTH_CLIENT_ID +
-  //           ":" +
-  //           process.env.RIOT_AUTH_CLIENT_SECRET
-  //       ).toString("base64")
-  //   );
-
-  //   // console.log(accessCode);
-
-  //   return NextResponse.json({ errore: error }, { status: 500 });
-  // }
+    return NextResponse.json({ token: tokens }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
 }
